38b2a992acb43b3b16cdcbf059d8dc71f2fadf1f
EduHoff <eduardohoffmanndocarmosilva@gmail.com>
2025-07-06 22:03:57 -0300

    alteracao da estetica e add de contador de loop

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
-var botao = document.getElementById('btn');
-var infanity_intro = new Audio('midia/infanity_intro.wav');
-var infanity_loop = new Audio('midia/infanity_loop.wav');
-var infanity_end = new Audio('midia/infanity_end.wav');
+const btn = document.querySelector('#btn');
+const loopCount = document.querySelector('#loopCount');
+let count = 0;
+let shouldLoop = true;
+
+const infanity_intro = new Audio('midia/infanity_intro.wav');
+const infanity_loop = new Audio('midia/infanity_loop.wav');
+const infanity_end = new Audio('midia/infanity_end.wav');
+
+
 
 function intro() {
 
-    botao.innerHTML = "Loading..."; // Altera o texto
-    botao.disabled = true;
-
-
+    btn.innerHTML = "Loading...";
+    loopCount.innerHTML = count;
+    btn.disabled = true;
+    btn.classList.add("bg-warning", "text-white");
     
+
     infanity_intro.play();
     infanity_intro.onended = function() {
     loop();
@@ -19,23 +26,32 @@ function intro() {
 
 function loop(){
     
-    botao.innerHTML = "Stop loop";
-    botao.disabled = false;
+    btn.innerHTML = "Stop loop";
+    count++;
+    loopCount.innerHTML = count;
+    btn.disabled = false;
+    btn.classList.remove("bg-warning", "text-white");
+    btn.classList.add("bg-success", "text-white");
 
-    
-    infanity_loop.loop = true;
     infanity_loop.play();
+
+    infanity_loop.onended = function () {
+        if (shouldLoop) {
+            count++;
+            loopCount.innerHTML = count;
+            infanity_loop.play();
+        } else {
+            infanity_end.play();
+        }
+    };
 }
 
 
 function end(){
     
-    infanity_loop.loop = false;
-    botao.disabled = true; 
-    botao.innerHTML = "Loop interrupted!";
-
-    infanity_loop.onended = function() {
-        infanity_end.play();
-        }
-
+    shouldLoop = false;
+    btn.disabled = true; 
+    btn.innerHTML = "Loop interrupted!";
+    btn.classList.remove("bg-success", "text-white");
+    btn.classList.add("bg-light", "text-dark");
 }
